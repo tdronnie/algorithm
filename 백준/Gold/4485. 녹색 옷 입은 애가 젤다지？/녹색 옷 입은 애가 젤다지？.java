329c7c7b201dd24553fb2383ca9686ca9170a807
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2023-10-17 22:16:13 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 236 ms, Memory: 19448 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,78 +1,92 @@
 import java.io.*;
-import java.util.*;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int[][] map, minus;
-    static boolean[][] visited;
-    static int[] moveR = {-1, 1, 0, 0};
-    static int[] moveC = {0, 0, -1, 1};
+	static int n, min = Integer.MAX_VALUE;
+	static int[][] map, minus;
+	static boolean[][] visited;
+	static int[] moveR = { -1, 1, 0, 0 };
+	static int[] moveC = { 0, 0, -1, 1 };
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test = 1;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int problem = 1;
+		while (true) {
+			n = Integer.parseInt(br.readLine());
+			StringBuilder sb = new StringBuilder();
+			if (n == 0)
+				return;
+			else {
+				min = Integer.MAX_VALUE;
+				map = new int[n][n];
+				minus = new int[n][n];
+				visited = new boolean[n][n];
+				for (int i = 0; i < n; i++) {
+					StringTokenizer st = new StringTokenizer(br.readLine());
+					for (int j = 0; j < n; j++) {
+						map[i][j] = Integer.parseInt(st.nextToken());
+						minus[i][j] = Integer.MAX_VALUE;
+					}
+				}
+				minus();
+				sb.append("Problem ").append(problem++).append(": ").append(min);
+				System.out.println(sb.toString());
 
-        //입력이 0이 들어올 때까지 계속 입력 받기
-        while (true) {
-            n = Integer.parseInt(br.readLine());
-            if (n == 0)
-                break;
+			}
+		}
+	}
 
-            map = new int[n][n];
-            visited = new boolean[n][n];
-            minus = new int[n][n];
+	private static void minus() {
+		
+		//오름차순으로 작은 루피를 가지는 좌표 부터 먼저 방문
+		PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				return o1[2] - o2[2];
+			}
+		});
+		minus[0][0] = map[0][0];
+		q.offer(new int[] { 0, 0, minus[0][0]});
+		
+		while (!q.isEmpty()) {
+			int[] val = q.poll();
+			int r = val[0];
+			int c = val[1];
+			int mr = val[2];
+			for (int k = 0; k < 4; k++) {
+				int newR = r + moveR[k];
+				int newC = c + moveC[k];
+				if (isValid(newR, newC) && !visited[newR][newC]) {
+					visited[r][c] = true;
+					
+					//잃는 루피가 더 없다면 minus배열 업데이트
+					if (minus[newR][newC] > mr + map[newR][newC]) {
+						minus[newR][newC] = mr + map[newR][newC];
+						q.offer(new int[] {newR, newC, mr+map[newR][newC]});
+					}
+					
+					//끝 좌표까지 왔을 경우 최솟값 업데이트
+					if(newR == n-1 && newC == n-1) {
+						min = Math.min(min, minus[newR][newC]);
+					}
+				}
+			}
 
-            for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                    minus[i][j] = Integer.MAX_VALUE; //다익스트라 초기화
-                }
-            }
+		}
 
-            bfs();
-            System.out.println("Problem " + test++ + ": " + minus[n - 1][n - 1]); //가장 마지막칸까지 가는데 가장 최소 비용(루피 조금만 잃음)
+	}
 
+	private static boolean isValid(int r, int c) {
+		if (r < 0 || c < 0 || r >= n || c >= n)
+			return false;
+		return true;
+	}
 
-        }
-    }
-
-    public static boolean isValid(int r, int c) {
-        if (r < 0 || r >= n || c < 0 || c >= n)
-            return false;
-        return true;
-    }
-
-    public static void bfs() {
-        Queue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[2]-o2[2];
-            }
-        });
-
-        q.add(new int[]{0, 0, map[0][0]}); //초기에 처음 좌표와 처음 좌표까지 가는 최단거리 큐에 넣기
-        minus[0][0] = map[0][0];
-
-        while (!q.isEmpty()) {
-            int[] val = q.poll();
-            int valR = val[0];
-            int valC = val[1];
-            int valMinus = val[2];
-
-            for (int k = 0; k < 4; k++) {
-                int newR = valR + moveR[k];
-                int newC = valC + moveC[k];
-
-                if (isValid(newR, newC)) {
-                    if (minus[newR][newC] > valMinus + map[newR][newC]) {
-                        minus[newR][newC] = valMinus + map[newR][newC];
-                        q.add(new int[]{newR, newC, minus[newR][newC]});
-                    }
-                }
-            }
-        }
-    }
 }
