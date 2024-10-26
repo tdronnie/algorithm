f5ab7d9d1c8148eebcd84d39d8320bcb1adb8f33
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2024-10-26 17:27:20 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 924 ms, Memory: 90084 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] orders;
+    static int order = 1;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N+1];
+        orders = new int[N+1];
+
+        for(int i=1; i<=N; i++){
+            graph[i] = new ArrayList<>();
+        }
+        visited = new boolean[N+1];
+        Arrays.fill(visited, false);
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for(int i=1; i<=N; i++){
+            Collections.sort(graph[i]);
+            Collections.reverse(graph[i]);
+        }
+
+        dfs(R);
+
+        for(int i=1; i<=N; i++){
+            sb.append(orders[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void dfs(int start){
+        visited[start] = true;
+        orders[start] = order++;
+
+        for(int i=0; i<graph[start].size(); i++){
+            int node = graph[start].get(i);
+            if(visited[node]) continue;
+            dfs(node);
+        }
+    }
+}
