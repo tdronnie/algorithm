c8b808bf5868f32c4bedf26785c43f83b4c54d96
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2024-11-23 00:08:34 +0900

    [level 2] Title: 최솟값 만들기, Time: 3.55 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int result = 0;
+        
+        for(int i=0; i<A.length; i++){
+            result += A[i] * B[B.length-1-i];
+        }
+        return result;
+        
+    }
+}
