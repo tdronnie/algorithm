22efa18b5d00dc6d0c3688a5376f10c04dac1c63
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2024-12-05 01:04:25 +0900

    [level 2] Title: 전화번호 목록, Time: 302.34 ms, Memory: 223 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> set = new HashSet<>();
+        
+        for(String s : phone_book){
+            set.add(s);
+        }
+        
+        for(String s : phone_book){
+            for(int i=0; i<s.length(); i++){
+                if(set.contains(s.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
