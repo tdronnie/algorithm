92f11e168630acf2a398458597cc74bb3ca3cf0d
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-04-11 18:55:06 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,6 +1,5 @@
 select animal_id, name,
-    case when sex_upon_intake like 'Neutered%' then 'O'
-        when sex_upon_intake like 'Spayed%' then 'O'
+    case when sex_upon_intake like 'Neutered%' or sex_upon_intake like 'Spayed%' then 'O'
         else 'X'
         end as '중성화'
 from animal_ins  
