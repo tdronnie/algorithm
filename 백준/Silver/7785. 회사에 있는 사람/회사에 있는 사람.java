f05bbbf0a0ed955daa5ec45216759100ffde2758
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-04-29 11:39:42 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 632 ms, Memory: 50148 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,38 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        HashMap<String, String> map = new HashMap<>(); //사람에 따른 출입기록 저장하기 위한 해시맵
-        StringBuilder sb = new StringBuilder(); //결과 출력 위한 sb
-        int n = Integer.parseInt(br.readLine());// 출입기록 수
+        Set<String> log = new HashSet<>();
 
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String person = st.nextToken();
-            String goInOut = st.nextToken();
-            if (goInOut.equals("leave")) { //퇴근할 경우 해시맵에서 삭제
-                map.remove(person); //key를 person으로 가지는 맵 항목 삭제
-            } else
-                map.put(person, goInOut); //사람과 enter 저장
+            String name = st.nextToken();
+            if (st.nextToken().equals("enter")) {
+                log.add(name);
+            } else {
+                log.remove(name);
+            }
         }
 
-        List<String> collect = map.keySet().stream() //keySet을 stream으로 변환
-                .sorted(Comparator.comparing(String::toString).reversed())//Comaparator의 메서드 comparing메서드참조를 통해 key의 String으로 받아 역순 정렬
-                .collect(Collectors.toList()); //리스트로 추출
-        for (String name :collect) { //이름들 사이 개행
-            sb.append(name+"\n");
+        ArrayList<String> enterNames = new ArrayList<>(log);
+        Collections.sort(enterNames, Collections.reverseOrder());
+
+        for (String name : enterNames) {
+            sb.append(name).append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        br.close();
-        bw.close();
+        System.out.println(sb);
+
+
     }
 }
