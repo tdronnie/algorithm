a2f59eb44da1062d75562006e18a140992085686
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-05-13 15:46:41 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,34 +1,51 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static boolean[] visited;
-	static int[] rslt;
 
-	public static void main(String[] args) throws IOException {
+    static int n, m;
+    static List<int[]> result;
+    static int[] nums;
+    static boolean[] visited;
+    static StringBuilder sb;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		rslt = new int[m]; // 결과 배열
-		combination(0, 1);
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        result = new ArrayList<>();
+        nums = new int[m];
+        visited = new boolean[n + 1];
 
-	}
+        NComM(0, 1);
 
-	public static void combination(int cnt, int start) {
-		if (cnt == m) {
-			for (int i = 0; i < m; i++)
-				System.out.print(rslt[i] + " ");
-			System.out.println();
-			return;
-		}
-		for (int i = start; i <= n; i++) {
-			rslt[cnt] = i;
-			combination(cnt + 1, i + 1);
-		}
-	}
+        System.out.println(sb);
+    }
 
+    static void NComM(int count, int start) {
+        if (count == m) {
+            for (int num : nums) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if(visited[i]) continue;
+
+            nums[count] = i;
+            visited[i] = true;
+            NComM(count+1, i);
+            visited[i] = false;
+        }
+    }
 }
