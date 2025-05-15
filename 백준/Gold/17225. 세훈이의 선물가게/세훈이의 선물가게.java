c80a7e6d35c12438ddf1e0687ae91f54bdec26e3
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-05-15 17:23:15 +0900

    [Gold V] Title: 세훈이의 선물가게, Time: 940 ms, Memory: 37568 KB, Score: 140 point -BaekjoonHub

--- 백준/Gold/17225. 세훈이의 선물가게/세훈이의 선물가게.java
+++ 백준/Gold/17225. 세훈이의 선물가게/세훈이의 선물가게.java
@@ -22,10 +22,23 @@ public class Main {
         }
     }
 
+    static class Order {
+        int arriveTime;
+        String color;
+        int num;
+
+        Order(int arriveTime, String color, int num) {
+            this.arriveTime = arriveTime;
+            this.color = color;
+            this.num = num;
+        }
+    }
+
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Stuff> stuffs = new ArrayList<>();
+        List<Order> orders = new ArrayList<>();
         List<Integer> Bs = new ArrayList<>();
         List<Integer> Rs = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -33,22 +46,40 @@ public class Main {
         int RPackTime = Integer.parseInt(st.nextToken());
         int customers = Integer.parseInt(st.nextToken());
 
+        int BEndTime = 0;
+        int REndTime = 0;
+
         for (int i = 0; i < customers; i++) {
             st = new StringTokenizer(br.readLine());
             int arriveTime = Integer.parseInt(st.nextToken());
             String color = st.nextToken();
             int num = Integer.parseInt(st.nextToken());
 
-            if(color.equals("B")){
-                for (int j = 0; j < num; j++) {
-                    stuffs.add(new Stuff("B", arriveTime + (j * BPackTime)));
-                }
-            }
+            orders.add(new Order(arriveTime, color, num));
+        }
 
-            if (color.equals("R")) {
+        for (Order order : orders) {
+            int arriveTime = order.arriveTime;
+            String color = order.color;
+            int num = order.num;
+            int start;
+
+            if (color.equals("B")) {
+                start = Math.max(arriveTime, BEndTime);
+
                 for (int j = 0; j < num; j++) {
-                    stuffs.add(new Stuff("R", arriveTime + (j * RPackTime)));
+                    stuffs.add(new Stuff("B", start));
+                    start += BPackTime;
                 }
+                BEndTime = start;
+            } else {
+                start = Math.max(arriveTime, REndTime);
+
+                for (int j = 0; j < num; j++) {
+                    stuffs.add(new Stuff("R", start));
+                    start += RPackTime;
+                }
+                REndTime = start;
             }
         }
 
