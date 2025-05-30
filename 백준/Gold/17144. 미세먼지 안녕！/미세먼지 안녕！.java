7637e416ae156b87a029a45799d0b0484aebc8ee
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-05-30 14:13:41 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 360 ms, Memory: 33100 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -4,132 +4,130 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int r, c;
-	static int[][] map, copy;
-	static int[] cleaner; //공기청정기가 위치하는 행들 저장
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		int t = Integer.parseInt(st.nextToken());
-		
-		map = new int[r][c];
-		
-		cleaner = new int[2];
-		int k=0;
-		for (int i = 0; i < r; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < c; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == -1) cleaner[k++] = i;
-			}
-		}
-		
-		//t초 만큼 진행
-		for (int i = 0; i < t; i++) {
-			//원 배열 복사해서 미세먼지 확산 적용
-			copy = new int[r][c];
-			for (int a = 0; a < r; a++) {
-				for (int b = 0; b < c; b++) {
-					copy[a][b] = map[a][b];
-				}
-			}
-			spread();
-			
-			//공기청정기로 인한 순환
-			ativate();
-			
-			//원 배열에 덮어쓰기
-			for (int a = 0; a < r; a++) {
-				for (int b = 0; b < c; b++) {
-					map[a][b] = copy[a][b];
-				}
-			}
-		}
-		
-		//t초 후 미세먼지 양 구하기
-		int cnt = 0;
-		for (int a = 0; a < r; a++) {
-			for (int b = 0; b < c; b++) {
-				if(map[a][b]>0) cnt += map[a][b];
-			}
-		}
-		System.out.println(cnt);
-	}
-	private static void ativate() {
-		//위쪽 반시계 방향으로 돌리기
-		int cr = cleaner[0];
-		for (int i = cr-1; i > 0 ; i--) {
-			copy[i][0] = copy[i-1][0];
-		}
-		for (int j = 0; j < c-1; j++) {
-			copy[0][j] = copy[0][j+1];
-		}
-		for (int i = 0; i < cr; i++) {
-			copy[i][c-1] = copy[i+1][c-1];
-		}
-		for (int j = c-1; j > 0; j--) {
-			copy[cr][j] = copy[cr][j-1];
-		}
-		copy[cr][1] = 0;
-		//아래쪽 시계방향으로 돌리기
-		cr = cleaner[1];
-		for (int i = cr+1; i < r-1; i++) {
-			copy[i][0] = copy[i+1][0];
-		}
-		for (int j = 0; j < c-1; j++) {
-			copy[r-1][j] = copy[r-1][j+1];
-		}
-		for (int i = r-1; i > cr; i--) {
-			copy[i][c-1] = copy[i-1][c-1];
-		}
-		for (int j = c-1; j > 0; j--) {
-			copy[cr][j] = copy[cr][j-1];
-		}
-		copy[cr][1] = 0;
-//		System.out.println("턴 진행 후!!!!!!!");
-//		print();
-		
-		
-		
-	}
-	private static void print() {
-		for (int a = 0; a < r; a++) {
-			for (int b = 0; b < c; b++) {
-				System.out.print(copy[a][b] + " ");
-			}
-			System.out.println();
-		}
-		
-	}
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0 ,1, 0, -1};
-	private static void spread() {
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				if(map[i][j]!=-1 && map[i][j]!=0) { //미세먼지인 경우
-					int cnt=0;
-					for (int k = 0; k < 4; k++) {
-						int newX = i + dx[k];
-						int newY = j + dy[k];
-						
-						if(newX<0 || newY<0 || newX>= r || newY >= c) continue;
-						if(map[newX][newY] == -1) continue;
-						
-						copy[newX][newY] += map[i][j]/5; //4방 갈 수 있는 곳에 미세먼지 뿌려주기
-						cnt++;
-					}
-					copy[i][j] -= map[i][j]/5*cnt; //뿌려지고 중심에 남은 미세먼지 
-				}
-				
-			}
-			
-		}
-		
-	}
 
+    static int row, col, time;
+    static int[][] room;
+    static int[] machine;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        time = Integer.parseInt(st.nextToken());
+        room = new int[row][col];
+        machine = new int[2];
+
+        int k = 0;
+
+        for (int i = 0; i < row; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < col; j++) {
+                room[i][j] = Integer.parseInt(st.nextToken());
+                if(room[i][j] == -1){
+                    machine[k] = i;
+                    k++;
+                }
+            }
+        }
+
+        start();
+        System.out.println(amountDust());
+    }
+
+    static void start() {
+        for(;time > 0; time--) {
+            expansion();
+            machineOn();
+        }
+    }
+
+    static void expansion() {
+        int[][] roomCp = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (room[i][j] != 0 && room[i][j] != -1) {
+                    int count = 0;
+
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+
+                        if(nx < 0 || ny <0 || nx >= row || ny >= col || room[nx][ny] == -1) continue;
+
+                        count++;
+                        roomCp[nx][ny] += room[i][j] / 5;
+                    }
+                    room[i][j] -= count * (room[i][j] / 5);
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                room[i][j] += roomCp[i][j];
+            }
+        }
+    }
+
+    static void machineOn() {
+        int upX = machine[0];
+
+        for (int i = upX - 2; i >= 0; i--) {
+            room[i + 1][0] = room[i][0];
+        }
+
+        for (int j = 1; j < col; j++) {
+            room[0][j - 1] = room[0][j];
+        }
+
+        for (int i = 1; i <= upX; i++) {
+            room[i - 1][col - 1] = room[i][col - 1];
+        }
+
+        for (int j = col - 2; j > 0; j--) {
+            room[upX][j + 1] = room[upX][j];
+        }
+        room[upX][1] = 0;
+
+        int downX = machine[1];
+
+        for (int i = downX + 2; i < row; i++) {
+            room[i - 1][0] = room[i][0];
+        }
+
+        for (int j = 1; j < col; j++) {
+            room[row - 1][j - 1] = room[row - 1][j];
+        }
+
+        for (int i = row - 2; i >= downX; i--) {
+            room[i + 1][col - 1] = room[i][col - 1];
+        }
+
+        for (int j = col - 2; j > 0; j--) {
+            room[downX][j + 1] = room[downX][j];
+        }
+        room[downX][1] = 0;
+
+    }
+
+    static int amountDust() {
+        int dust = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (room[i][j] != 0 && room[i][j] != -1) {
+                    dust += room[i][j];
+                }
+            }
+        }
+        return dust;
+    }
 }
