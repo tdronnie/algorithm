16b75cb2ab94442809cf852b7766e449446ae162
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-08-25 16:41:08 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.35 ms
+메모리: 83.5 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 14일 23:43:50
+2025년 08월 25일 16:41:05
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
-        
         boolean[] visited = new boolean[n];
         int count = 0;
         
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                count++;
-                detectArea(visited, n, computers, i);
-            }
+            if(visited[i]) continue;
+            Queue<Integer> q = new ArrayDeque<>();
+            q.add(i);
+            visited[i] = true;
+            
+            while(!q.isEmpty()){
+                int next = q.poll();
                 
+                for(int j=0; j<n; j++){
+                    if(visited[j] || computers[next][j] == 0) continue;
+                    
+                    visited[j] = true;
+                    q.add(j);
+                }
+            }
+            
+            count++;     
         }
         return count;
-    }
-    
-    public void detectArea(boolean[] visited, int n, int[][] computers, int start){
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        visited[start] = true;
-        
-        while(!q.isEmpty()){
-            int com = q.poll();
-            
-            for(int i=0; i<n; i++){
-                if(computers[com][i] == 0 || visited[i]) continue;
-                
-                visited[i] = true;
-                q.add(i);
-            }
-        }
     }
 }
