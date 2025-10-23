1952a5a981b4236b70b0bbb643d58da47684ef3b
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-10-23 21:26:30 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,4 +1,9 @@
+/*
+한 문자열 안에 다른 문자열이 순서대로 들어가는지
+선행 스킬(알파벳)을 배우면 no++, 타겟 문자열을 순회하면서 스킬트리 안에 있는 스킬이라면 현재 no와 실제 해당 스킬순서가 맞는지 확인
+*/
 class Solution {
+
     public int solution(String skill, String[] skill_trees) {
         
         int possible = 0;
