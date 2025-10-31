5307670bd36ba9ce4a71f720cfab90e83a394ec3
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2025-10-31 23:10:48 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,9 @@
+select id,
+case
+    when ntile(4) over (order by size_of_colony) = 1 then 'LOW'
+    when ntile(4) over (order by size_of_colony) = 2 then 'MEDIUM'
+    when ntile(4) over (order by size_of_colony) = 3 then 'HIGH'
+    else 'CRITICAL'
+end as 'COLONY_NAME'
+from ecoli_Data
+order by id
