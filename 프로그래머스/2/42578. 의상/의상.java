3997fe9020ffb2efcd5cb3311592e670f8963931
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-04-18 19:29:37 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int kinds = 1;
+        Map<String, Integer> kinds = new HashMap<>(); // 중복 의상 없으므로 단순 개수로 세기
+        int value = 1; // 종류 경우의 수를 위한 변수
         
-        for(int i=0; i<clothes.length; i++){
-            String kind = clothes[i][1];
-            map.put(kind, map.getOrDefault(kind, 0) + 1);
+        // 종류에 따른 의상 개수 저장
+        for(String[] pair  : clothes){
+            kinds.put(pair[1], kinds.getOrDefault(pair[1], 0) + 1);
         }
         
-        if(map.size() == 1) return clothes.length;
-        
-        for(int count : map.values()){
-            kinds *= (count + 1);
+        // 종류가 한가지인 경우
+        if(kinds.size() == 1) {
+            return clothes.length; // 각 의상 수
         }
-        return kinds - 1;
+        
+        for(int count : kinds.values()){
+            value *= (count + 1); // 종류 의상 입지 않는 경우까지 + 1해서 모든 경우의 수 구해주기
+        }
+        return value - 1; // 모든 의상 안입는 1가지 제외
     }
 }
