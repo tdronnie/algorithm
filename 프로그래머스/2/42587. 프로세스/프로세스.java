c258a9355b6f8e87509658c3c936c4c6f485e94d
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-04-26 01:14:25 +0900

    [level 2] Title: 프로세스, Time: 0.94 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,33 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int[] sorts = priorities.clone();
-        Arrays.sort(sorts);
-        Queue<int[]> q = new ArrayDeque<>();
+        
+        Deque<int[]> q = new ArrayDeque<>();
+        int[] sortPri = priorities.clone(); // 우선순위대로 처리 위해 정렬 배열
+        Arrays.sort(sortPri);
+        int idx = sortPri.length - 1; // 오름차순 정렬로 뒷순서부터 중요도 높음
+        int count = 0; // 처리 순서 카운트
         
         for(int i=0; i < priorities.length; i++){
-            if(i == location){
-                q.add(new int[]{priorities[i], 1});
-            } else {
-                q.add(new int[]{priorities[i], 0});
+            q.addLast(new int[]{priorities[i], i}); // 각 프로세스 별 (우선순위, 초기 인덱스) 저장
+        }
+        
+        while(!q.isEmpty()){
+            
+            int qSize = q.size(); // 현재 존재하는 큐 요소 개수만큼 프로세스 실행
+            
+            for(int i=0; i<q.size(); i++){
+                int[] process = q.pollFirst();
+                if(process[0] == sortPri[idx]){ // 현재 처리 우선순위라면 처리 수행
+                    idx--; // 다음 높거나 같은 우선순위 실행
+                    count++;
+                    if(process[1] == location) { // 찾는 프로세스 위치라면 바로 리턴
+                        return count;
+                    }
+                } else { // 현재 처리 우선순위 아니라면 대기 큐 넣기
+                    q.addLast(process);
+                }
             }
         }
         
-        int idx = sorts.length-1;
-        int turn = 1;
-        while(!q.isEmpty()){
-            if(q.peek()[0] == sorts[idx]){
-                idx--;
-                int[] value = q.poll();
-                if(value[1] == 1){
-                    return turn;
-                }
-                turn++;
-                q.add(value);
-            } else {
-                q.add(q.poll());
-            }
-        }
         return 0;
+
     }
 }
