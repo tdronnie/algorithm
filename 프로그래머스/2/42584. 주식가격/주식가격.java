0e6a763018df6692a242cbe673ecec9e794bf7f4
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-04-26 15:37:25 +0900

    [level 2] Title: 주식가격, Time: 30.87 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,25 +1,29 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        Stack<Integer> st = new Stack<>();
+        
+        Deque<Integer> dq = new ArrayDeque<>();
         int[] answer = new int[prices.length];
         
         for(int i=0; i<prices.length; i++){
-            while(!st.isEmpty() && prices[st.peek()] > prices[i]){
-                int lowStart = st.pop();
-                answer[lowStart] = i - lowStart; 
+            
+            // 가격이 떨어지는 상황의 경우, 지금까지 저장된 이전 초 떨어지지 않은 기간 계산
+            while(!dq.isEmpty() && prices[dq.peekFirst()] > prices[i]){
+                int pre = dq.pollFirst();
+                answer[pre] = i - pre;
             }
-            st.push(i);
+            
+            // 가격이 떨어지지 않는 동안 잠시 저장
+            dq.addFirst(i);
         }
         
-        //뒤에 더 낮은 가격이 나오지 않았던 인덱스 처리
+        // 뒤에 낮은 가격 나오지 않았던 부분 처리
         for(int i=0; i<prices.length; i++){
             if(answer[i] == 0){
-                answer[i] = prices.length - i - 1;
+                answer[i] = prices.length - 1 - i;
             }
         }
         
         return answer;
-        
     }
 }
