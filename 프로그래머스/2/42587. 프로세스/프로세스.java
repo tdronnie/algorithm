3bcc14d723fdba46e4115ad4d8d3c53569a62f31
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-04-30 21:38:36 +0900

    [level 2] Title: 프로세스, Time: 1.11 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,36 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        Deque<int[]> q = new ArrayDeque<>();
-        int[] sortPri = priorities.clone(); // 우선순위대로 처리 위해 정렬 배열
-        Arrays.sort(sortPri);
-        int idx = sortPri.length - 1; // 오름차순 정렬로 뒷순서부터 중요도 높음
-        int count = 0; // 처리 순서 카운트
-        
-        for(int i=0; i < priorities.length; i++){
-            q.addLast(new int[]{priorities[i], i}); // 각 프로세스 별 (우선순위, 초기 인덱스) 저장
+        for (int p : priorities) {
+            pq.add(p);
         }
-        
-        while(!q.isEmpty()){
-            
-            int qSize = q.size(); // 현재 존재하는 큐 요소 개수만큼 프로세스 실행
-            
-            for(int i=0; i<q.size(); i++){
-                int[] process = q.pollFirst();
-                if(process[0] == sortPri[idx]){ // 현재 처리 우선순위라면 처리 수행
-                    idx--; // 다음 높거나 같은 우선순위 실행
-                    count++;
-                    if(process[1] == location) { // 찾는 프로세스 위치라면 바로 리턴
-                        return count;
+
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    
+                    if (i == location) {
+                        return answer;
                     }
-                } else { // 현재 처리 우선순위 아니라면 대기 큐 넣기
-                    q.addLast(process);
                 }
             }
         }
-        
-        return 0;
-
+        return answer;
     }
 }
