72931a9e14589e8e6515de0a308f00ca131866dc
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-06 23:55:26 +0900

    [level 2] Title: 더 맵게, Time: 1486.40 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,36 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 스코빌 지수기준 오름차순 정렬
         int count = 0;
-        int up = 0;
-        for(int i=0; i<scoville.length; i++){
-            if(scoville[i] >= K){
-                up++;
-            }
-            pq.add(scoville[i]);
+        
+        // 음식 정렬
+        for(int food : scoville){
+            pq.add(food);
         }
         
-        if(up == scoville.length){
-            return 0;
-        }
-        
-        while(!pq.isEmpty()){
+        // 섞을 수 있는 두 음식이 존재하고 가장 낮은 음식의 스코빌 지수가 K미만인 동안 음식 섞기
+        while(pq.size() > 1 && pq.peek() < K){
+            int l1 = pq.poll(); // 가장 맵지 않은 음식의 스코빌 지수
+            int l2 = pq.poll(); // 두번째로 맵지 않은 음식의 스코빌 지수
             
-            int l1 = pq.poll();
-            if(l1 >= K){
-                break;
-            }
-            if(pq.isEmpty()){
-                return -1;
-            }
+            pq.add(l1 + (l2*2)); // 음식 섞기
+            count++; // 섞기 카운트
             
-            int l2 = pq.poll();
-            int mix = l1 + (l2*2);
-            pq.add(mix);
-            count++;
         }
         
-        return count;
+        // 섞기 후 가장 맵지 않은 음식의 스코빌 지수가 K 미만이라면 실패
+        if(pq.peek() < K){
+            return -1;
+        } else {
+            return count;
+        }
+        
     }
 }
