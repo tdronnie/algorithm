f3316a48a1e2609fd376fc405fc408ecf2fdc26a
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-10 14:48:25 +0900

    [level 2] Title: 롤케이크 자르기, Time: 153.32 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,24 +1,39 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
-        HashMap<Integer, Integer> m1 = new HashMap<>();
-        HashMap<Integer, Integer> m2 = new HashMap<>();
+        
+        Map<Integer, Integer> b1 = new HashMap<>();
+        Map<Integer, Integer> b2 = new HashMap<>();
         int count = 0;
         
-        for(int i=0; i<topping.length; i++){
-            m1.put(topping[i], m1.getOrDefault(topping[i], 0) + 1);
+        //첫번째 각 가진 종류 카운트
+        b1.put(topping[0], 1);
+        for(int i=1; i<topping.length; i++){
+            b2.put(topping[i], b2.getOrDefault(topping[i], 0) + 1);
         }
         
-        for(int t : topping){
-            m1.put(t, m1.get(t) - 1);
-            if(m1.get(t) == 0){
-                m1.remove(t);
-            }
-            m2.put(t, m2.getOrDefault(t, 0) + 1);
+        //공평하면 카운트
+        if(b1.size() == b2.size()){
+            count++;
+        }
+        
+        // 가장 앞에부터 잘라가기
+        for(int i=1; i<topping.length-1; i++){
             
-            if(m1.size() == m2.size()) count++;
+            b1.put(topping[i], b1.getOrDefault(topping[i], 0));
+            if(b2.containsKey(topping[i])){
+                if(b2.get(topping[i]) - 1 == 0){
+                    b2.remove(topping[i]);
+                } else {
+                    b2.put(topping[i], b2.get(topping[i]) - 1);    
+                }
+            }
+            
+            //공평하면 카운트
+            if(b1.size() == b2.size()){
+                count++;
+            }
         }
-        
         return count;
     }
 }
