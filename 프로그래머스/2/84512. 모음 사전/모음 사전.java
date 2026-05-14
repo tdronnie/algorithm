64a81e3bd5aafb89c74c4d5b44ae9627e660524b
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-14 19:14:47 +0900

    [level 2] Title: 모음 사전, Time: 13.96 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,23 +2,26 @@ import java.util.*;
 class Solution {
     
     private final String VOWELS = "AEIOU";
+    static List<String> words;
     
     public int solution(String word) {
-        List<String> words = new ArrayList<>();
+        words = new ArrayList<>();
         
-        dfs(0, "", words);
+        dfs(0, ""); // 사전 만들기
         
         return words.indexOf(word) + 1;
     }
     
-    public void dfs(int wordCount, String tempWord, List<String> words){
+    public void dfs(int wordCount, String tempWord){
         if(wordCount == 5){
             return;
         }
+        
+        // 사전순으로 알파벳 하나씩 더하면서 모든 경우의 수 저장
         for(int i=0; i<5; i++){
             String newTempWord = tempWord + VOWELS.charAt(i);
             words.add(newTempWord);
-            dfs(wordCount+1, newTempWord, words);
+            dfs(wordCount+1, newTempWord);
         }
     }
 }
