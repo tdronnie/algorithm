2415aa58877f9bb7ffabe6591d455dbd655a12c0
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-14 19:31:53 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int area = brown + yellow;
+        int area = brown + yellow; // 카펫의 면적
         
-        //가로 세로 임의로 구하기
+        //세로 = i(최소 3), 가로 area/i로 두고 작은 면적부터 대입
         for(int i=3; i<=area; i++){
             int width = area/i;
             //노랑 면적은 (width-2) * (i-2)
