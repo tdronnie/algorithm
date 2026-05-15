010e1a209a4ade6b419c1409a1311372de5d0750
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-16 02:04:10 +0900

    [level 2] Title: 타겟 넘버, Time: 2.83 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,22 @@
 class Solution {
-    static int count = 0; // 방법의 수
+    
+    int count = 0;
     public int solution(int[] numbers, int target) {
         
-        // 주어진 숫자를 넣거나 빼거나 해서 타겟 숫자 만들기
-        makeNumber(0, 0, numbers, target);
+        makeTarget(numbers, 0, 0, target);
         return count;
     }
     
-    public void makeNumber(int sum, int idx, int[] numbers, int target){
-        if(idx == numbers.length){
-            if(sum == target){
+    public void makeTarget(int[] numbers, int idx, int current, int target){
+        
+        if(idx == numbers.length){ // 모든 숫자 사용했다면 target이 되었는지 확인
+            if(current == target){
                 count++;
             }
             return;
         }
         
-        makeNumber(sum + numbers[idx], idx + 1, numbers, target);
-        makeNumber(sum - numbers[idx], idx + 1, numbers, target);
+        makeTarget(numbers, idx + 1, current + numbers[idx], target);
+        makeTarget(numbers, idx + 1, current - numbers[idx], target);
     }
 }
