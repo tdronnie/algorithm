e412df78c6957488e10c61dff0228e485bcc4be5
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-16 15:55:31 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,30 @@
 import java.util.*;
 class Solution {
+    int count = 0;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
-        int count = 0;
+        
+        // 모든 노드에 대해서 네트워크 탐색
+        visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(visited[i]) continue; // 이전 네트워크에 속하지 않은 방문하지 않은 컴퓨터라면 탐색 시작
+            visited[i] = true;
+            findNetwork(i, n, computers, visited);
+            count++; // 네트워크 개수++
+        }
+        
+        return count;
+        
+    }
+    
+    public void findNetwork(int start, int n, int[][] computers, boolean[] visited){
         
         for(int i=0; i<n; i++){
-            if(visited[i]) continue;
-            Queue<Integer> q = new ArrayDeque<>();
-            q.add(i);
-            visited[i] = true;
-            
-            while(!q.isEmpty()){
-                int next = q.poll();
-                
-                for(int j=0; j<n; j++){
-                    if(visited[j] || computers[next][j] == 0) continue;
-                    
-                    visited[j] = true;
-                    q.add(j);
-                }
+            // 같은 컴퓨터가 아니고, 연결된 컴퓨터, 방문하지 않았다면 탐색
+            if(i != start && computers[start][i] == 1 && !visited[i]){ 
+                visited[i] = true;
+                findNetwork(i, n, computers, visited);    
             }
-            
-            count++;     
         }
-        return count;
     }
 }
