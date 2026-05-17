8fb923d282ac3354d5c4b8a073e16821a172bc4f
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-17 23:13:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.86 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 52.4 MB, 시간: 9.89 ms
+메모리: 57.3 MB, 시간: 3.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 14일 23:10:46
+2026년 05월 17일 23:13:19
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,37 @@
 import java.util.*;
 class Solution {
     
-    static int row, col;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     public int solution(int[][] maps) {
-        row = maps.length;
-        col = maps[0].length;
         
-        return findShortRoute(maps);
-        
-    }
-    
-    static int[] moveX = {-1, 0, 1, 0};
-    static int[] moveY = {0, 1, 0, -1};
-
-    public static int findShortRoute(int[][] maps){
-        boolean[][] visited = new boolean[row][col];
+        // BFS로 상대 팀 진영까지의 최단거리 구하기
         Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         q.add(new int[]{0, 0, 1});
         visited[0][0] = true;
         
         while(!q.isEmpty()){
             int[] loc = q.poll();
             
-            if(loc[0] == row-1 && loc[1] == col-1){
+            // 적 진영인 경우 지금까지 지나온 칸의 개수 리턴
+            if(loc[0] == (maps.length -1) && loc[1] == (maps[0].length -1)){
                 return loc[2];
             }
             
-            for(int i=0; i<4; i++){
-                int newX = loc[0] + moveX[i];
-                int newY = loc[1] + moveY[i];
-                int step = loc[2];
+            for(int k=0; k<4; k++){
+                int nx = loc[0] + dx[k];
+                int ny = loc[1] + dy[k];
                 
-                if(newX < 0 || newY < 0 || newX >= row || newY >= col || maps[newX][newY] == 0 || visited[newX][newY]) continue;
+                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length || visited[nx][ny] || maps[nx][ny] == 0) continue;
+                visited[nx][ny] = true;
                 
-                visited[newX][newY] = true;
-                q.add(new int[]{newX, newY, step+1});
+                q.add(new int[]{nx, ny, loc[2] + 1});
             }
-            
         }
+        
+        // 상대 팀 진영에 도착할 수 없는 경우 -1
         return -1;
+
     }
 }
