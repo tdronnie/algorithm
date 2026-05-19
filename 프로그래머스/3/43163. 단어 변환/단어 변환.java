c96e09c2c963f098e56e410c117f2450de24639f
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-19 20:59:06 +0900

    [level 3] Title: 단어 변환, Time: 0.70 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,91 +1,67 @@
 import java.util.*;
+class Word {
+    String word;
+    int count = 0; // 거치는 횟수 카운트
+    
+    public Word(String word, int count){
+        this.word = word;
+        this.count = count;
+    }
+}
 class Solution {
-    static int min = Integer.MAX_VALUE;
+    
+    List<String>[] arr;
     public int solution(String begin, String target, String[] words) {
-        ArrayList<String>[] list = new ArrayList[words.length + 2];
-        HashMap<String, ArrayList<String>> map = new HashMap<>();
-        Set<String> visited = new HashSet<>();
-        boolean flag = false;
         
-        map.put(begin, new ArrayList<>());
-        for(int i=0; i<words.length; i++){
-            map.put(words[i], new ArrayList<>());
-            if(!flag && words[i].equals(target)){
-                flag = true;
-            }
-        }
-        if(!flag){
-            return 0;
-        }
+        Map<String, ArrayList<String>> map = new HashMap<>(); // <String, ArrayList<String>> 저장 map
+        Queue<Word> q = new ArrayDeque<>(); // bfs
+        Set<String> visited = new HashSet<>(); // 방문처리 set
         
-        for(String word : map.keySet()){
-            for(String next : map.keySet()){
-                if(word.equals(next)) continue;
-                if(diffCount(word, next)){
-                    map.get(word).add(next);
+        
+        // begin 포함 단어 관계 저장
+        String[] all = Arrays.copyOf(words, words.length + 1);
+        all[words.length] = begin;
+
+        for(int i=0; i<all.length - 1; i++){
+            for(int j=i+1; j <all.length; j++){
+                // 한글자만 다른 경우 관계 잇기
+                if(canMove(all[i], all[j])){
+                    map.computeIfAbsent(all[i], k -> new ArrayList<>()).add(all[j]);
+                    map.computeIfAbsent(all[j], k -> new ArrayList<>()).add(all[i]);
                 }
             }
         }
         
-        
-        
-//         for(String word : map.keySet()){
+        q.add(new Word(begin, 0));
+        visited.add(begin);
+        while(!q.isEmpty()){
+            Word current = q.poll();
             
-//             Set<Character> chs = new HashSet<>();
-//             for(char ch : word.toCharArray()){
-//                 chs.add(ch);
-//             }
+            // target 변환 완료
+            if(current.word.equals(target)){
+                return current.count;
+            }
             
-//             for(String next : map.keySet()){
-//                 Set<Character> union = new HashSet<>(chs);
-//                 Set<Character> common = new HashSet<>(chs);
-//                 Set<Character> nexts = new HashSet<>();
-                
-//                 if(word.equals(next)) continue;
-//                 for(char ch : next.toCharArray()){
-//                     nexts.add(ch);
-//                 }
-                
-//                 union.addAll(nexts);
-//                 common.retainAll(nexts);
-
-//                 if(word.length() - common.size() <= 1){
-//                     map.get(word).add(next);
-//                 }
-//             }
-//         }
-        
-        findShortRoute(begin, target, map, visited, 0);
-        
-        if(min == Integer.MAX_VALUE){
-            return 0;
-        }
-        return min;
-    }
-    
-    void findShortRoute(String word, String target, HashMap<String, ArrayList<String>> map, Set<String> visited, int count){
-        
-        if(visited.contains(word)) return;
-        visited.add(word);        
-        
-        if(word.equals(target)){
-            min = Math.min(min, count);
-            return;
-        }
-        for(String next : map.get(word)){
-            findShortRoute(next, target, map, visited, count + 1);
-        }
-    }
-    
-    boolean diffCount(String s1, String s2){
-        int diffCount = 0;
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)){
-                diffCount++;
-                if(diffCount > 1) return false;
+            for(String next : map.get(current.word)){
+                if(visited.contains(next)) continue;
+                visited.add(next);
+                q.add(new Word(next, current.count + 1));
             }
         }
         
+        // 변환 불가
+        return 0;
+         
+    }
+    // 두 문자열이 한글자만 다른 경우 true 리턴
+    public boolean canMove(String s1, String s2){
+        int count = 0;
+        for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                if(count > 1) return false;
+            }
+        }
         return true;
     }
 }
