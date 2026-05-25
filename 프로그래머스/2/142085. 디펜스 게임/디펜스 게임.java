6fec7046a7b9caf93b14c5132ec9f8b624e87def
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-05-25 12:36:10 +0900

    [level 2] Title: 디펜스 게임, Time: 66.38 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,63 +1,27 @@
 import java.util.*;
 class Solution {
-    
-    int maxRound;
     public int solution(int n, int k, int[] enemy) {
         
+        // 갔다가ㅏ 불가능한 라운드 만나면 이전 라운드 중 적 수 가장 많은 라운드에 무적권 사용 (최대한 많은 병사 확보)
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        // 라운드 진행하다가 적이 많은 순서대로 진행하다가 끝난 경우 다시 돌아가서 무적권 처리로 세이브
-        for(int i=0; i< enemy.length; i++){
+        for(int i=0; i<enemy.length; i++){
             n -= enemy[i];
-            pq.add(enemy[i]); // 나중에 돌아와서 무적권 쓸 수도 있으니 세이브
+            pq.add(enemy[i]);
             
-            // 현재 라운드 진행 불가
+            // 현재 라운드 통과 불가
             if(n < 0){
-                // 저장해놓았던 턴 중 적이 가장 많은 라운드에 무적권 사용
-                if(k > 0){
-                    n += pq.poll(); // 병사 살리기
+                // 무적권 사용할 수 있으면
+                if (k > 0){
+                    n += pq.poll();
                     k--;
-                } else { // 사용할 무적권 없다면 라운드 종료
+                } else { // 무적권 없다면 현재 라운드에서 게임종료
                     return i;
-                }          
+                }
             }
         }
         
-        return enemy.length;
-        
-        
-        
-        
-        
-        
-//         maxRound = -1;
-        
-//         playGame(n, k, 0, enemy);
-        
-        // return maxRound;
-        
-    }
-    
-    public void playGame(int remain, int card, int round, int[] enemy){
-        
-        // 모든 라운드 완료
-        if(round == enemy.length){
-            maxRound = round;
-            return;
-        }
-        
-        // 무적권 사용
-        if(card > 0){
-            playGame(remain, card - 1, round + 1, enemy);    
-        }
-        
-        // 막을 수 있는 경우
-        if(remain >= enemy[round]){
-            playGame(remain - enemy[round], card, round+1, enemy);    
-        } else { // 막을 수 없는 경우, 이전 라운드까지 가능
-            maxRound = Math.max(maxRound, round);
-            return;
-        }
+        return enemy.length; // 모든 라운드 막아냄
         
     }
 }
