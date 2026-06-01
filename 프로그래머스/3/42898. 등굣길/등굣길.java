5dce12f1ea5950557d62b8df1dfba2d63bdd0cf1
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-06-01 23:57:23 +0900

    [level 3] Title: 등굣길, Time: 0.58 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,38 @@
 class Solution {
+    
+    int[] dx = {0, 1};
+    int[] dy = {1, 0};
     public int solution(int m, int n, int[][] puddles) {
         
-        int[][] map = new int[n][m];
-        int[][] move = new int[n][m];
-        move[0][0] = 1;
-        
-        for(int i=0; i<puddles.length; i++){
-            map[puddles[i][1]-1][puddles[i][0]-1] = 1;
+        int[][] map = new int[n+1][m+1];
+        for(int i=0; i< puddles.length; i++){
+            map[puddles[i][1]][puddles[i][0]] = 1;            
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(map[i][j] == 1) continue;
-                if(i > 0) move[i][j] = (move[i][j] + move[i-1][j]) % 1000000007;
-                if(j > 0) move[i][j] = (move[i][j] + move[i][j-1]) % 1000000007;
+        int[][] shortest = new int[n+1][m+1];
+        shortest[1][1] = 1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(i == 1 && j == 1 || map[i][j] == 1) continue;
+                
+                // 두번째 행부터 윗행 최단경로 개수 더해주기
+                if(i > 1){
+                    shortest[i][j] += shortest[i-1][j];
+                }
+                
+                // 두번째 열부터 왼쪽열 최단경로 개수 더해주기
+                if(j > 1){
+                    shortest[i][j] += shortest[i][j-1];    
+                }
+                
+                // 나머지 구하기
+                shortest[i][j] %= 1000000007;
+                
             }
         }
         
-        return move[n-1][m-1];
+        return shortest[n][m];
         
     }
 }
