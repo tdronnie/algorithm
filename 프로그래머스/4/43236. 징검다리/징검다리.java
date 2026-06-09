3d5e726128f70ced91e8472d54861af3c38fc096
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-06-10 00:35:54 +0900

    [level 4] Title: 징검다리, Time: 15.06 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -2,39 +2,37 @@ import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         Arrays.sort(rocks);
-        // 바위간 최소 간격 중 최댓값
-        int left = 1;
-        int right = 1000000000;
+        // 바위 간 거리 이분탐색
+        int min = 1;
+        int max = 1000000000;
         
-        while(left <= right){
-            int mid = (left+right) / 2;
-            if(isPossible(mid, rocks, n, distance)){
-                left = mid + 1;
-            } else {
-                right = mid -1;
-            }
-        }
-        
-        return right; //가능한 최대 간격
-    }
-    
-    boolean isPossible(int mid, int[] rocks, int n, int distance){
-        int pre = 0;
-        int removed = 0;
-        
-        for(int pos : rocks){
-            if(pos - pre < mid){ // 돌 사이 간격 mid보다 작으면
-                removed++;
-            } else {
-                pre = pos;
+        while(min <= max){
+            int mid = (min + max) / 2;
+            int removed = 0;
+            int pre = 0;
+            for(int r : rocks){
+                if(r - pre < mid){ // 바위 간 거리가 mid보다 작은 경우 바위없애기
+                    removed++;
+                } else { // 바위 간 거리가 mid보다 크거나 같은 경우 다음 바위로 넘어가기
+                    pre = r;
+                }
+                
+                // n보다 많이 지워진 경우 더 좁혀야 덜 지우기 가능
+                if(removed > n){
+                    break;
+                }
             }
             
-            if(removed > n) return false;
+            if(distance - pre < mid) removed++; // 마지막 바위와 도착지점 비교
+            
+            if(removed > n){
+                max = mid - 1;    
+            } else {
+                min = mid + 1;
+            }
         }
         
-        //마지막 바위와의 간격 확인
-        if(distance - pre < mid) removed++;
+        return max;
         
-        return removed <= n;
     }
 }
