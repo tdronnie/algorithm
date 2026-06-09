afbfdf6c641b5e14b3a13bdbc3c2c6d425fed17c
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-06-09 15:37:26 +0900

    [level 2] Title: 타겟 넘버, Time: 4.46 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,20 +3,33 @@ class Solution {
     int count = 0;
     public int solution(int[] numbers, int target) {
         
-        makeTarget(numbers, 0, 0, target);
+        // 타겟까지 남은 양 구하기 위한 numbers 합 구하기
+        int total = 0;
+        for(int num : numbers){
+            total += num;
+        }
+            
+        makeTarget(numbers, 0, 0, total, target);
         return count;
     }
     
-    public void makeTarget(int[] numbers, int idx, int current, int target){
+    public void makeTarget(int[] numbers, int idx, int current, int remain, int target){
         
-        if(idx == numbers.length){ // 모든 숫자 사용했다면 target이 되었는지 확인
+        // 남은 숫자를 조합해도 target 만들지 못하는 경우
+        if(Math.abs(target-current) > remain){
+            return;
+        }
+        
+        // 모든 숫자 사용했다면 target이 되었는지 확인
+        if(idx == numbers.length){ 
             if(current == target){
                 count++;
             }
             return;
         }
         
-        makeTarget(numbers, idx + 1, current + numbers[idx], target);
-        makeTarget(numbers, idx + 1, current - numbers[idx], target);
+        // 인덱스 순으로 더하거나 빼거나
+        makeTarget(numbers, idx + 1, current + numbers[idx], remain - numbers[idx], target);
+        makeTarget(numbers, idx + 1, current - numbers[idx], remain - numbers[idx], target);
     }
 }
