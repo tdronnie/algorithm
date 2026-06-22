d4df92340d9fa52e088b8882d08488ad02e74070
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-06-23 01:04:57 +0900

    [level 2] Title: 완전범죄, Time: 0.33 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,43 +1,41 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] info, int n, int m) {
-        // dp[i][j] = A 흔적 i, B 흔적 j가 가능한지 여부
-        boolean[][] dp = new boolean[n][m];
-        dp[0][0] = true;
-
-        for (int[] stuff : info) {
-            int a = stuff[0];
-            int b = stuff[1];
-            boolean[][] next = new boolean[n][m];
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (!dp[i][j]) continue;
-
-                    // A가 훔치는 경우, A 흔적 합 i + a < n
-                    if (i + a < n) {
-                        next[i + a][j] = true;
-                    }
-
-                    // B가 훔치는 경우, B 흔적 합 j + b < m
-                    if (j + b < m) {
-                        next[i][j + b] = true;
-                    }
-                }
-            }
-            dp = next;
+        
+        int[][] dp = new int[info.length + 1][m]; // dp[턴][b의 흔적누적값] = a의 최소 흔적누적값
+        for(int[] value : dp){
+            Arrays.fill(value, Integer.MAX_VALUE);    
         }
-
-        // A의 흔적 최소인 것 찾기
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dp[i][j]) {
-                    return i;
+        
+        dp[0][0] = 0; // 0번째 b가 0개 훔친 경우 a의 최솟값 0
+        
+        for(int i=0; i<info.length; i++){
+            for(int j=0; j<m; j++){
+                
+                if(dp[i][j] == Integer.MAX_VALUE){ // 현재 경우에 올 수 없는 경우 넘어가기
+                    continue;
                 }
+                
+                // a가 훔쳐도 붙잡히지 않는경우 업데이트
+                if(dp[i][j] + info[i][0] < n){
+                    dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j] + info[i][0]);    
+                }
+                
+                // b가 훔쳐도 붙잡히지 않는경우 업데이트
+                if(j + info[i][1] < m){
+                    dp[i+1][j + info[i][1]] = Math.min(dp[i+1][j + info[i][1]], dp[i][j]);    
+                }
+                
             }
         }
-
-        return -1;
+        
+        // 모든 b누적값에 대해 마지막 턴의 a누적 최솟값 구하기
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<m; i++){
+            min = Math.min(min, dp[info.length][i]);
+        }
+        
+        return min == Integer.MAX_VALUE ? -1 : min;
+        
     }
 }
