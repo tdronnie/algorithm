414b51770cd810d47658699332b3278229581ebc
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-08-02 00:34:48 +0900

    [level 2] Title: 디펜스 게임, Time: 56.89 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,26 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         
-        // 갔다가ㅏ 불가능한 라운드 만나면 이전 라운드 중 적 수 가장 많은 라운드에 무적권 사용 (최대한 많은 병사 확보)
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i=0; i<enemy.length; i++){
-            n -= enemy[i];
+        int answer = -1;
+        int i=0;
+        for(;i < enemy.length; i++){
+            
             pq.add(enemy[i]);
             
-            // 현재 라운드 통과 불가
-            if(n < 0){
-                // 무적권 사용할 수 있으면
-                if (k > 0){
-                    n += pq.poll();
-                    k--;
-                } else { // 무적권 없다면 현재 라운드에서 게임종료
+            if(n < enemy[i]){
+                
+                if(pq.isEmpty() || k <= 0){
                     return i;
                 }
+                n += pq.poll();
+                k--;
             }
+            n -= enemy[i];
         }
         
-        return enemy.length; // 모든 라운드 막아냄
+        if(i == enemy.length){
+            return enemy.length;
+        }
         
+        return i + 1;
     }
 }
