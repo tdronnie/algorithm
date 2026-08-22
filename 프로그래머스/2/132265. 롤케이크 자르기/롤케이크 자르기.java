66870b0db51704aab47bf60a0e0b47883689c581
SeEun Lee <69182630+tdronnie@users.noreply.github.com>
2026-08-22 23:48:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 47.38 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,38 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         
-        Map<Integer, Integer> b1 = new HashMap<>();
-        Map<Integer, Integer> b2 = new HashMap<>();
+        int[] part1 = new int[10001];
+        int part1Kinds = 0;
+        Set<Integer> part2 = new HashSet<>();
         int count = 0;
         
-        //첫번째 각 가진 종류 카운트
-        b1.put(topping[0], 1);
-        for(int i=1; i<topping.length; i++){
-            b2.put(topping[i], b2.getOrDefault(topping[i], 0) + 1);
+        for(int t : topping){
+            if(part1[t] == 0) part1Kinds++; // 종류 수만 체크
+            part1[t]++;
         }
         
-        //공평하면 카운트
-        if(b1.size() == b2.size()){
-            count++;
-        }
-        
-        // 가장 앞에부터 잘라가기
-        for(int i=1; i<topping.length-1; i++){
+        // 앞에서 하나씩 파니셔닝
+        for(int t : topping){
+            part2.add(t);
+            part1[t]--;            
+            if(part1[t] == 0) part1Kinds--;
             
-            b1.put(topping[i], b1.getOrDefault(topping[i], 0));
-            if(b2.containsKey(topping[i])){
-                if(b2.get(topping[i]) - 1 == 0){
-                    b2.remove(topping[i]);
-                } else {
-                    b2.put(topping[i], b2.get(topping[i]) - 1);    
-                }
-            }
-            
-            //공평하면 카운트
-            if(b1.size() == b2.size()){
+            if(part1Kinds == part2.size()){
                 count++;
             }
         }
+        
         return count;
+        
     }
 }
